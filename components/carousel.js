7302530838934a028752f1c2b16707b8483061c2
BenJerke <appliedboredom@gmail.com>
2022-07-11 13:44:00 -0500

    layout changes

--- components/carousel.js
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-import { buildCarousel } from '../utils/utils';
-
-
-export default function Carousel ({ images }) {
-    
-}
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getCarouselPicData } from '../utils/utils'
-import React, { useState, useEffect } from 'react'
-
+import React, { useState } from 'react'
+import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 
 export async function getStaticProps() {
@@ -28,32 +28,52 @@ export default function Home({ picSrc }) {
 const sleep = (delay) => new Promise ((resolve) => setTimeout(resolve, delay));
 
 const [image, setImage] = useState(`/images/product_images/${picSrc[0]}`);
-const [imgClass, setImgClass] = useState(null);
+const [imgLeft, setImgLeft] = useState(`/images/product_images/${picSrc[(picSrc.length - 1)]}`)
+const [imgRight, setImgRight] = useState(`/images/product_images/${picSrc[1]}`)
+const [imgClass, setImgClass] = useState(styles.image);
+const [imgLeftClass, setImgLeftClass] = useState(styles.image);
+const [imgRightClass, setImgRightClass] = useState(styles.image);
 const [btnUnclickable, setBtnUnclickable] = useState(null);
 
 function triggerLeftSlide() {
   setImgClass(styles.imageslidefromright);
+  setImgLeftClass(styles.imageslidefromright);
+  setImgRightClass(styles.imageslidefromright)
 }
 
 function triggerRightSlide() {
   setImgClass(styles.imageslidefromleft);
+  setImgLeftClass(styles.imageslidefromleft);
+  setImgRightClass(styles.imageslidefromleft);
 }
 
 function resetAnimationClass() {
   setImgClass(styles.image);
+  setImgLeftClass(styles.image);
+  setImgRightClass(styles.image);
 }
 
-async function changePicRight() {  
+async function changePicRight() {
   let oldPath = image.replace("/images/product_images/", "")
   let x = picSrc.indexOf(oldPath);  
-  let y = (picSrc.length - 1) ;
+  let y = (picSrc.length - 1);
+  let a = 0;
+  let b = 0;
   if (x < y) {
     x += 1;
+    a = x - 1;
+    b = x + 1;
   } else if (x == y){
     x = 0;
+    a = y;
+    b = x + 1;
   }
 
+  
+
   setImage(`/images/product_images/${picSrc[x]}`);
+  setImgLeft(`/images/product_images/${picSrc[a]}`);
+  setImgRight(`/images/product_images/${picSrc[b]}`);
   triggerLeftSlide();
   setBtnUnclickable(true);
   await sleep(1100);
@@ -65,13 +85,22 @@ async function changePicLeft() {
   let oldPath = image.replace("/images/product_images/", "")
   let x = picSrc.indexOf(oldPath);
   let y = (picSrc.length - 1);
-  if (x > 0) {
+  let a = 0;
+  let b = 0;
+  if (x > 0 && x != y) {
+    b = x + 1
+    a = x; 
     x -= 1;
+    
+    
+    
   } else {
     x = y;
   }
 
   setImage(`/images/product_images/${picSrc[x]}`);
+  setImgLeft(`/images/product_images/${picSrc[a]}`);
+  setImgRight(`/images/product_images/${picSrc[b]}`);
   triggerRightSlide();
   setBtnUnclickable(true);
   await sleep(1100);
@@ -87,31 +116,54 @@ async function changePicLeft() {
         <meta name="description" content="not done don't look aaaaa" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar bg="light" expand="lg">
+      <Container>
+      <Navbar.Brand href="/">Ash Stitches</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav >
+          <Nav.Link className="justify-content-end" href="/about">About</Nav.Link>
+          <Nav.Link className="justify-content-end" href="/contact">Contact</Nav.Link>
+          <Nav.Link className="justify-content-end" href="/gallery">Gallery</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
       <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Main Title
-        </h1>
-        <h3>
-          Subtitle
-        </h3>
-        <div>
-              <div>
                 <div>
-                  <button id="left" className={styles.button} disabled={btnUnclickable} onClick={changePicLeft}> 👈 </button>  
+                  <Image
+                    id="imgLeft"
+                    src={imgLeft}
+                    className={imgLeftClass}
+                    alt='left product'
+                    width='400'
+                    height='400'
+                  />                  
 
                   <Image
                   id="image"        
                   src={image} 
                   className={imgClass}                
-                  alt='product' 
+                  alt='central product' 
                   width='400'   
                   height='400'
                   />
-                  <button id="right" className={styles.button} onClick={changePicRight} disabled={btnUnclickable}> 👉 </button> 
+                
+                  <Image
+                    id="imgRight"                    
+                    src={imgRight}
+                    className={imgRightClass}
+                    alt='right product'
+                    width='400'
+                    height='400'
+                  />
                 </div>
-              </div>
-        </div>
+                <div className={styles.container}>
+                <Button id="left" className={styles.button} disabled={btnUnclickable} onClick={changePicLeft}> 👈 </Button>  
+                <Button id="right" className={styles.button} onClick={changePicRight} disabled={btnUnclickable}> 👉 </Button> 
+                </div>
+
       </main>
       </div>
     </div>       
