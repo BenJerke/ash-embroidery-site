958d7edbaba8c0298aeed980feaf16a9aadcb201
BenJerke <appliedboredom@gmail.com>
2022-09-17 13:34:23 -0400

    link fix

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ export default function About() {
       <Navbar.Toggle className={styles.navlinks} aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className={styles.navlinks} id="basic-navbar-nav">
         <Nav className={styles.navlinks}>
-          <Nav.Link href="https://www.instagram.com/ash_lewie/">Instagram</Nav.Link>
+          <Nav.Link href="https://www.instagram.com/ash_embroiders/">Instagram</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="mailto:embroideryash@gmail.com">Contact</Nav.Link>
         </Nav>
--- pages/index.js
+++ pages/index.js
@@ -163,7 +163,7 @@ async function changePicLeft() {
       <Nav className={styles.navlinks}>
         <Nav.Link href="/about">About</Nav.Link>
         <Nav.Link href="mailto: embroideryash@gmail.com">Contact</Nav.Link>      
-        <Nav.Link href="https://www.instagram.com/ash_lewie/">Instagram</Nav.Link>
+        <Nav.Link href="https://www.instagram.com/ash_embroiders/">Instagram</Nav.Link>
       </Nav>
     </Navbar.Collapse>
     </Container>
