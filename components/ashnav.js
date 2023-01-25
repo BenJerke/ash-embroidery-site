c6125dccabc7259384d08ead896e060705b71ddd
BenJerke <appliedboredom@gmail.com>
2023-01-25 10:10:12 -0500

    fix insta link

--- components/ashnav.js
+++ components/ashnav.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 import React, { useState, useEffect, useCallback } from 'react'
 import { Button, Navbar, Container, Nav, NavDropdown, Row, Col } from 'react-bootstrap'
 
@@ -19,7 +20,7 @@ return (
     <Nav className={styles.navlinks}>
       <Nav.Link href="/about">About</Nav.Link>
       <Nav.Link href="mailto: embroideryash@gmail.com">Contact</Nav.Link>      
-      <Nav.Link href="https://www.instagram.com/ash_lewie/">Instagram</Nav.Link>
+      <Nav.Link href="https://www.instagram.com/ash_embroiders/">Instagram</Nav.Link>
       <Nav.Link href="/gallery">Gallery</Nav.Link>
     </Nav>
     </Navbar.Collapse>
